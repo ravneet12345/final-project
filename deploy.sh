86539c5ea6b17f0af8a9b5f79e18639fc818ccdc
ravneet12345 <95903908+ravneet12345@users.noreply.github.com>
2025-06-10 21:17:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-docker login
-docker build -t react-ecommerce-app
-docker tag react-ecommerce-app:latest ravneeth123/react-ecommerce-app:latest
-docker push ravneeth123/react-ecommerce-app:latest
+docker-compose up -d
 
